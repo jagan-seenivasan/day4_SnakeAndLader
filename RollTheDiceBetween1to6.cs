e6fddd5260e949efa0b222d8b54e7a385397abad
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-10 20:05:17 +0530

    Add files via upload

---
+++ RollTheDiceBetween1to6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class RollTheDiceBetween1to6
+    {
+        public static void RollDicein1to6()
+        {
+            int player = 1;
+           
+
+            Random random = new Random();
+            int DiceRoll = random.Next(1, 7);
+
+            Console.WriteLine("No. of Player in there " + player);
+
+            Console.WriteLine("You got number :" + DiceRoll);
+            Console.ReadLine();
+        }
+    }
+}
