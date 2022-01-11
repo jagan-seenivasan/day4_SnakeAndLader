af9e3a45d08ad7610d08e27f0c10b35763a598aa
jagan-seenivasan <85672216+jagan-seenivasan@users.noreply.github.com>
2022-01-11 12:42:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckPlayerPosition Uc1 = new CheckPlayerPosition();
+            CheckPlayerPosition.CheckThePlayerPosition();
+        //    RollTheDiceBetween1to6 Uc2 = new RollTheDiceBetween1to6();
+        //    RollTheDiceBetween1to6.RollDicein1to6();
+        //    RepeatTillWinIn100Pos Uc3 =new RepeatTillWinIn100Pos();
+        //    RepeatTillWinIn100Pos.RepeatTillWin();
+        //    ExactWinningPositionAt100 Uc4 = new ExactWinningPositionAt100();
+        //    ExactWinningPositionAt100.RepeatTillWinningAtPosition100();
+        //    StartedFromPositionZero uc5 = new StartedFromPositionZero();
+        //    StartedFromPositionZero.initialzePlayerAtPositionZero();
+        //    NoOfTimesDiceRoll Uc6 = new NoOfTimesDiceRoll();
+         //   NoOfTimesDiceRoll.CheckNoOfTimesDiceRoll();
+        }
+    }
+}
